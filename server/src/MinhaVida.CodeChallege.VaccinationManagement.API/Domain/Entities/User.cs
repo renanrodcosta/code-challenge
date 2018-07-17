a8b75273e5afdd35138731c123d477e1aac178fe
Renan Costa <renan.costa@ltm.digital>
2018-07-17 00:27:29 -0300

    Model user

--- server/src/MinhaVida.CodeChallege.VaccinationManagement.API/Domain/Entities/User.cs
+++ server/src/MinhaVida.CodeChallege.VaccinationManagement.API/Domain/Entities/User.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace MinhaVida.CodeChallege.VaccinationManagement.API.Domain.Entities
 {
     public class User
     {
-        
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
