64d8d30c58197adfb23aed2c760154e4c8f01466
Renan Costa <renan.costa@ltm.digital>
2018-07-17 00:27:48 -0300

    Creating repository people

--- server/src/MinhaVida.CodeChallege.VaccinationManagement.API/Domain/Repositories/PeopleRepository.cs
+++ server/src/MinhaVida.CodeChallege.VaccinationManagement.API/Domain/Repositories/PeopleRepository.cs
@@ -1,7 +1,28 @@
+using MinhaVida.CodeChallege.VaccinationManagement.API.Domain.Entities;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
 namespace MinhaVida.CodeChallege.VaccinationManagement.API.Domain.Repositories
 {
     public class PeopleRepository
     {
-        
+        private IMongoCollection<Person> people;
+
+        public PeopleRepository(MongoClient client)
+        {
+            var database = client.GetDatabase("mongodbdotnetcore");
+            people = database.GetCollection<Person>(nameof(people));
+        }
+
+        public async Task AddAsync(Person person) =>
+            await people.InsertOneAsync(person);
+
+        public async Task UpdateAsync(Person person)
+        {
+            var x = await people.FindOneAndUpdateAsync(p => p.Id == person.Id, person);
+        }
+
+        public async Task DeleteAsync(string id) =>
+            await people.FindOneAndDeleteAsync(p => p.Id == id);
     }
 }
